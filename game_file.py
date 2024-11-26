00adfddc2302341d6d0a326a78e21a05cd7a85c9
skippka <145149359+skippka@users.noreply.github.com>
2024-11-26 09:50:54 +0200

    Update game_file.py

--- game_file.py
+++ game_file.py
@@ -1,12 +1,35 @@
 import json
 
-def read_from_file():
-    try:
-        with open('data.json', 'r') as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return {'balance': 1000, 'gameplay': 0}
+FILE_NAME = "users_data.json"
 
-def write_in_file(data):
-    with open('data.json', 'w') as file:
+def get_user_data(user_id):
+    """
+    Получить данные пользователя по его ID.
+    Если данных нет, возвращается начальный баланс и игровая статистика.
+    """
+    try:
+        with open(FILE_NAME, 'r') as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+
+    if str(user_id) not in data:
+        data[str(user_id)] = {"balance": 1000, "gameplay": 0}
+        update_user_data(user_id, data[str(user_id)])
+
+    return data[str(user_id)]
+
+def update_user_data(user_id, user_data):
+    """
+    Обновить данные пользователя в файле.
+    """
+    try:
+        with open(FILE_NAME, 'r') as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+
+    data[str(user_id)] = user_data
+
+    with open(FILE_NAME, 'w') as file:
         json.dump(data, file, indent=4)
